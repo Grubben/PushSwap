fcad6d58a5322151f46d5290a34652b5f9a39864
Grubben <antoniomsprc@gmail.com>
2022-07-11 23:49:49 +0100

    bgear: algo working wonderfully, thanks be to God! Not working for 3 2 1

--- bgearSort.py
+++ bgearSort.py
@@ -12,31 +12,38 @@ def bgearSort(a, b):
             r(a, "a") # rra might be more efficient in some cases
             moves += 1
         return moves
+
     if switchedP(a, is_ascendingP) > (-1):
             return swap_at(a, switchedP(a, is_ascendingP), "a")
+
+    chunks = len(a) // 10
+    if len(a) > 100:
+        chunks = 100
 
     p(b, a, "b")
     moves += 1
 
     while a:
-        if turnedP(a):
-            print("Turned.")
-            moves += set_ascending(a)
-            print("Unturned a")
+        # if turnedP(a):
+        #     print("Turned.")
+        #     moves += set_ascending(a)
+        #     print("Unturned a")
         
-#        if switchedP(a, is_ascendingP) > (-1):
-#            swap_at(a, switchedP(a, is_ascendingP), "a")
+        # if switchedP(a, is_ascendingP) > (-1):
+        #    swap_at(a, switchedP(a, is_ascendingP), "a")
         
         # if a is in order and everything is bigger than in b
         # then there is no need to pb
         # Just order b and pa!
-        elif sorted(a) == a and min(a) > max(b):
+        if sorted(a) == a and min(a) > max(b):
             moves += drain(b, a, "a")
             return moves
 
+        # Put smallest number on top by rev/rotating
+        prepTop(a, chunks)
 
         # Top of A is smaller than everything in B
-        elif a[0] < min(b):
+        if a[0] < min(b):
             moves += set_descending(b)
             p(b, a, "b")
             r(b, "b")

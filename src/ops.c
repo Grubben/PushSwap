b27fbe7a94df702dd21ba233faf9c3b091b17af0
Grubben <antoniomsprc@gmail.com>
2022-07-30 15:43:46 +0100

    ops.c: fixed s func

--- src/ops.c
+++ src/ops.c
@@ -1,18 +1,23 @@
 #include "../header/push_swap.h"
 
+//Swap the first 2 elements at the top of stack
 int s(t_list **stack)
-//TODO: TBC and verified
 {
-	t_list *newFirst;
+
+	t_list	*oldFirst;
+	t_list	*newFirst;
 
 	if (stack && ft_lstsize(*stack) > 1)
 	{
-		newFirst = (*stack)->next;
-		ft_lstadd_back(stack, *stack);
-		stack = &newFirst;
+		oldFirst = *stack;
+		newFirst = (*stack) -> next;
+		oldFirst -> next = newFirst -> next;
+		newFirst -> next = oldFirst;
+		*stack = newFirst;
 		return (1);
 	}
 	return (0);
+
 }
 
 int	p(t_list **stack1, t_list **stack2)
@@ -60,5 +65,35 @@ int	rr(t_list **stack)
 		newTail -> next = NULL;
 		newHead -> next = *stack;
 		*stack = newHead;
+		return (1);
 	}
+	return (0);
+}
+
+int	main(void)
+{
+	t_list	*head;
+	t_list	el;
+
+	head = NULL;
+
+	el.content = (void *)"good";
+	el.next = NULL;
+
+	printf("%d\n", ft_lstsize(head)); // == 0
+
+	head = ft_lstnew((void *)"hello");
+	printf("%d\n", ft_lstsize(head)); // == 1
+
+	ft_lstadd_front(&head, &el);
+	printf("%d\n", ft_lstsize(head)); // == 2
+
+	ft_lstadd_back(&head, ft_lstnew((void *)"bye"));
+	printf("%d\n", ft_lstsize(head)); // == 2
+
+	ft_lstprint(head);
+
+	// Check if s func works
+	printf("%d\n", s(&head));
+	ft_lstprint(head);
 }

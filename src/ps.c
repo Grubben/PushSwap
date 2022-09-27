c157897143429923f80b4e58c8b8dcf73b1118f9
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r5s1.42lisboa.com>
2022-09-27 11:05:31 +0100

    src/ps:bettered. Still not defended though

--- src/ps.c
+++ src/ps.c
@@ -6,7 +6,7 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 10:05:38 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/09/27 10:18:15 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/09/27 10:54:46 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ int main(int argc, char **argv)
 {
     t_list  *args, *b;
     int     i, count;
+    int     *tmp;
 
-    args = ft_lstnew(ft_atoi(argv[0]));
+    if (argc < 2)
+        return (0);
+    tmp = ft_calloc(1, sizeof(int));
+    args = ft_lstnew(tmp);
     i = 1;
     while (argv[i])
     {
-        ft_lstadd_back(&args, ft_lstnew(ft_atoi(argv[i])));
+        tmp = ft_calloc(1, sizeof(int));
+        *tmp = ft_atoi(argv[i]);
+        ft_lstadd_back(&args, ft_lstnew(tmp));
         i++;
     }
     b = NULL;

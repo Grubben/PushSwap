37f1dcd5fad5b4f6ab6fe95ccee16642f4f4574e
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r5s1.42lisboa.com>
2022-09-27 09:18:55 +0100

    header/push_swap.h: Added stackops funcs to header

--- header/push_swap.h
+++ header/push_swap.h
@@ -45,4 +45,42 @@ int	rr(t_list **stack);
 int	revRotate(t_list **stack, int howMany);
 
 
+/*	STACKOPS	*/
+
+int	is_ascendingP(t_list *stack);
+
+int	is_descendingP(t_list *stack);
+
+int	set_ascending(t_list **stack);
+
+int	set_descending(t_list **stack);
+
+/*
+ * Drains all nums in giver to receiver with p
+ */
+int	drain(t_list **giver, t_list **receiver);
+
+/*
+ * Returns whether the stack is Turned or not
+ */
+int	turnedP(t_list *stack);
+
+/*
+ * A switched stack is in the order specified by
+ * verifierF with a single swap. VerifierF is a predicate
+ */
+int	switchedP(t_list *stack, int (*f)(t_list *));
+
+/*
+ * Swaps at given location
+ */
+int	swap_at(t_list *stack, int index);
+
+/*
+ * Rotates the given stack to get the more efficient
+ * lower number to the top
+ */
+size_t	prepTop(t_list *stack);
+
+
 # endif
